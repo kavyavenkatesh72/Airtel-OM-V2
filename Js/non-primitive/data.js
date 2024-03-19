0582ec623462fc0727953af2b02a2bbc97c27bdc
kavyavenkatesh72 <kavyav3560@gmail.com>
2024-03-19 22:40:55 +0530

    txt

---
+++ Js/non-primitive/data.js
@@ -0,0 +1,10 @@
+var enames=["rahul","sonia","priya","Modi"]
+var emp={
+    id:101,
+    name:"rahul",
+    sal:45000
+}
+console.log(enames)
+console.log(typeof enames)
+console.log(emp)
+console.log(typeof emp)
