baaffddcf5040535162c41a5d5ae07c78d2a8975
kavyavenkatesh72 <kavyav3560@gmail.com>
2024-03-19 22:14:15 +0530

    text

---
+++ Js/crud/one.js
@@ -0,0 +1,15 @@
+//create
+var id=101;
+
+//read
+console.log(id)
+
+//update
+id=201
+
+console.log(id)
+
+//delete
+delete id
+
+console.log(id)
---
+++ Js/crud/two.js
@@ -0,0 +1,4 @@
+var id=101
+delete id
+
+console.log(id)
